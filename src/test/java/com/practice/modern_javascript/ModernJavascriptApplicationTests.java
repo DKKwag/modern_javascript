5f0e819625b5edc8782abc49f6b62c69040719a5
DKKwag <im.kdk93@gmail.com>
2023-03-07 13:16:14 +0900

    Initial commit

---
+++ src/test/java/com/practice/modern_javascript/ModernJavascriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.modern_javascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavascriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
